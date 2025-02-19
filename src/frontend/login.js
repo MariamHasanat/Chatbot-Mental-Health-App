51eea3afe8cf1d278b88ffceef2fabfd0f0dec13
Mariam Hasanat <hasanattmariam@gmail.com>
2025-02-19 08:49:05 +0200

    edit the logo in the changeMode button

--- src/frontend/login.js
+++ src/frontend/login.js
@@ -13,16 +13,13 @@ showPasswordCheckbox.addEventListener('change', () => {
 
 if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.add('dark-theme');
-    themeToggle.textContent = '☀️';
 }
 
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains('dark-theme')) {
         localStorage.setItem('theme', 'dark');
-        themeToggle.textContent = '☀️';
     } else {
         localStorage.setItem('theme', 'light');
-        themeToggle.textContent = '🌙';
     }
 });
