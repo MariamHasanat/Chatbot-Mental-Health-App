c049cb37da7a2652075e82a4f8ecff2c5a162d76
Mariam Hasanat <hasanattmariam@gmail.com>
2025-02-19 17:31:40 +0200

    fix error in navigation on start conversation button

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,12 +1,3 @@
-import { startConversation } from './js/scripts.js';
-
 import './styles/login.scss';
 import './styles/styles.scss';
 import './assets/chat-bot-bro-1.svg';
-
-window.Client = window.Client || {}; 
-window.Client.startConversation = startConversation; 
-
-console.log("Client is now available globally:", window.Client);
-
-export { startConversation };
--- src/frontend/js/scripts.js
+++
@@ -1,5 +0,0 @@
-function startConversation() {
-    window.location.href = './login.html'; // because we are building the project with the dist folder as the root, all the files will be in the same path as the index.html file
-}
-
-export { startConversation };
