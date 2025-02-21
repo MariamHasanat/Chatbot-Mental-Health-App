0e6a8807badb9a26c10b72fdcddb6b736e0a99ab
Mariam Hasanat <hasanattmariam@gmail.com>
2025-02-21 02:35:12 +0200

    add and connect pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -53,6 +53,22 @@ module.exports = {
             template: "./src/frontend/views/home-page.html",
             filename: "./home-page.html",
         }),
+        new HtmlWebPackPlugin({
+            template: "./src/frontend/views/articles.html",
+            filename: "./articles.html",
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/frontend/views/chat.html",
+            filename: "./chat.html",
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/frontend/views/emergency.html",
+            filename: "./emergency.html",
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/frontend/views/settings.html",
+            filename: "./settings.html",
+        }),
         new WorkboxPlugin.GenerateSW({
             clientsClaim: true,
             skipWaiting: true,
