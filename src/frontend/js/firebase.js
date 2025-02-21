c0cdeda3bb5492842867a224b940591ede2bd09c
Mariam Hasanat <hasanattmariam@gmail.com>
2025-02-21 21:33:07 +0200

    save data

--- src/frontend/js/firebase.js
+++ src/frontend/js/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9pzctZ7A6-SdAy_-_4zESmn8r9S1ztFo",
-  authDomain: "chatbot-mental-health-app.firebaseapp.com",
-  projectId: "chatbot-mental-health-app",
-  storageBucket: "chatbot-mental-health-app.firebasestorage.app",
-  messagingSenderId: "888144826527",
-  appId: "1:888144826527:web:bee0a112b37fe601a852aa",
-  measurementId: "G-3VHV3BTNT8"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
